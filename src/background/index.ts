597528af2be0c42ec7b031dd0ad67a94a52efdc9
fentosv <fentos.dev@gmail.com>
2024-01-27 20:39:18 +0100

    Refactor onInstall methods

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,26 +1,6 @@
-import { SEARCH_PLACEHOLDER } from '../constants'
 import onInstall from '../utils/onInstall'
 import onMessage from '../utils/onMessage'
 import onStorageChanged from '../utils/onStorageChanged'
-
-const searchObj: { id: string; url: string }[] = [
-  { id: '🌐 Google', url: `https://google.com/search?q=${SEARCH_PLACEHOLDER}` },
-  { id: '🤖 StackOverflow', url: `https://stackoverflow.com/search?q=${SEARCH_PLACEHOLDER}` },
-  { id: '💻 GitHub Code', url: `https://github.com/search?q=${SEARCH_PLACEHOLDER}&type=code` },
-  { id: '📚 GitHub Repositories', url: `https://github.com/search?q=${SEARCH_PLACEHOLDER}&type=repositories` },
-]
-
-// Open a new search tab when the user clicks a context menu
-chrome.contextMenus.onClicked.addListener((item, tab) => {
-  const tabIndex = tab!.index
-  const selectionText = item.selectionText!
-  const menuItemId = item.menuItemId
-  const selectedSearchUrl = searchObj.find(({ id }) => id === menuItemId)?.url
-  if (!selectedSearchUrl) return
-  const url = selectedSearchUrl.replace(SEARCH_PLACEHOLDER, selectionText)
-
-  chrome.tabs.create({ url: url, index: tabIndex + 1 })
-})
 
 console.log('Service worker started.')
 
--- src/utils/onInstall.ts
+++ src/utils/onInstall.ts
@@ -1,13 +1,14 @@
-import { STORAGE_KEYS } from '../constants'
+import { SEARCH_PLACEHOLDER, STORAGE_KEYS } from '../constants'
 import { DEFAULT } from './presets'
 import { setStorage } from './storage'
 
-async function saveInitialDate() {
-  await setStorage({ [STORAGE_KEYS.installDate]: installDate })
+async function initStorage() {
+  await setStorage({ [STORAGE_KEYS.searchLinks]: DEFAULT.searchLinks })
 }
 
 async function onInstallCallback(details: chrome.runtime.InstalledDetails) {
-  console.log('Installed ✅', details)
+  console.log('Installed ✅', details.reason)
+  initStorage()
   for (const { id } of DEFAULT.searchLinks) {
     chrome.contextMenus.create({
       id: id,
@@ -16,6 +17,17 @@ async function onInstallCallback(details: chrome.runtime.InstalledDetails) {
       contexts: ['selection'],
     })
   }
+  // Open a new search tab when the user clicks a context menu
+  chrome.contextMenus.onClicked.addListener((item, tab) => {
+    const tabIndex = tab!.index
+    const selectionText = item.selectionText!
+    const menuItemId = item.menuItemId
+    const selectedSearchUrl = DEFAULT.searchLinks.find(({ id }) => id === menuItemId)?.url
+    if (!selectedSearchUrl) return
+    const url = selectedSearchUrl.replace(SEARCH_PLACEHOLDER, selectionText)
+
+    chrome.tabs.create({ url: url, index: tabIndex + 1 })
+  })
 }
 
 export default function onInstall() {
