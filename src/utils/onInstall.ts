495d713bea64440120a36fd07117466cdefd882b
fentosv <fentos.dev@gmail.com>
2024-02-01 13:20:01 +0100

    Implement addContextMenu in onInstall

--- src/utils/onInstall.ts
+++ src/utils/onInstall.ts
@@ -1,6 +1,7 @@
-import { SEARCH_PLACEHOLDER, STORAGE_KEYS } from '../constants'
 import { DEFAULT } from './presets'
 import { setStorage } from './storage'
+import { STORAGE_KEYS } from '../constants'
+import addContextMenu from './addContextMenu'
 
 async function initStorage() {
   await setStorage({ [STORAGE_KEYS.searchLinks]: DEFAULT.searchLinks })
@@ -9,25 +10,7 @@ async function initStorage() {
 async function onInstallCallback(details: chrome.runtime.InstalledDetails) {
   console.log('Installed ✅', details.reason)
   initStorage()
-  for (const { id } of DEFAULT.searchLinks) {
-    chrome.contextMenus.create({
-      id: id,
-      title: id,
-      type: 'normal',
-      contexts: ['selection'],
-    })
-  }
-  // Open a new search tab when the user clicks a context menu
-  chrome.contextMenus.onClicked.addListener((item, tab) => {
-    const tabIndex = tab!.index
-    const selectionText = item.selectionText!
-    const menuItemId = item.menuItemId
-    const selectedSearchUrl = DEFAULT.searchLinks.find(({ id }) => id === menuItemId)?.url
-    if (!selectedSearchUrl) return
-    const url = selectedSearchUrl.replace(SEARCH_PLACEHOLDER, selectionText)
-
-    chrome.tabs.create({ url: url, index: tabIndex + 1 })
-  })
+  addContextMenu(DEFAULT.searchLinks)
 }
 
 export default function onInstall() {
