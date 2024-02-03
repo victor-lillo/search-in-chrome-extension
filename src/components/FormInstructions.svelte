2bf2c234d9fafdd035dbbfe789dff92bed9aa19d
fentosv <fentos.dev@gmail.com>
2024-02-03 12:07:40 +0100

    Add FormInstructions component

---
+++ src/components/FormInstructions.svelte
@@ -0,0 +1,40 @@
+<details>
+  <summary>Instructions</summary>
+  <ul>
+    <li><strong>ID:</strong> Add the ID you want.</li>
+    <li>
+      <strong>URL:</strong> Add the URLS. It must start with <code>https://</code> and must contain
+      <code>PLACEHOLDER</code>, which will be replaced when used.
+    </li>
+  </ul>
+</details>
+
+<style>
+  details {
+    border: 1px solid #aaa;
+    border-radius: var(--border-radius-1);
+    padding: 0.5em 0.5em 0;
+    cursor: pointer;
+    list-style-type: '▶️';
+  }
+
+  summary {
+    font-weight: bold;
+    margin: -0.5em -0.5em 0;
+    padding: 0.5em;
+  }
+
+  details[open] {
+    list-style-type: '🔽';
+    padding: 0.5em;
+  }
+
+  details[open] summary {
+    border-bottom: 1px solid #aaa;
+    margin-bottom: 0.5em;
+  }
+
+  li {
+    margin-left: 1rem;
+  }
+</style>
