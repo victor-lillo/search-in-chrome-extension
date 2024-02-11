10fb5b1a57902907e96dc49c082c3f23f4de67cc
fentosv <fentos.dev@gmail.com>
2024-02-11 20:03:28 +0100

    Clean up Popup component

--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
   import Button from '../components/Button.svelte'
   import Form from '../components/Form.svelte'
-  import { MESSAGE_OBJECT } from '../constants'
-  import { sendMessageToBackground, sendMessageToContent } from '../utils/sendMessage'
-
-  export let prop: string
 
   const openOptionsPage = () => {
     chrome.runtime.openOptionsPage()
   }
-
-  const messageToBackground = async () => {
-    const response = await sendMessageToBackground({ [MESSAGE_OBJECT.key]: MESSAGE_OBJECT.value })
-    console.log('POPUP: Response from background:', response)
-  }
-
-  const messageToContent = async () => {
-    const response = await sendMessageToContent({ [MESSAGE_OBJECT.key]: MESSAGE_OBJECT.value })
-    console.log('POPUP: Response from content:', response)
-  }
 </script>
 
 <section>
-  Popup {prop}
-  <Button handleClick={messageToBackground} variant="alert">Send message to Background</Button>
-  <Button handleClick={messageToContent} variant="alert">Send message to Content</Button>
   <Button handleClick={openOptionsPage}>Open options</Button>
   <Form />
 </section>
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,12 +1,10 @@
 import '../styles/app.css'
-import { extensionName } from './../utils/extensionData'
 import Popup from './Popup.svelte'
 
-const target = document.getElementById('app')
+const target = document.getElementById('app')!
 
 async function render() {
-  const prop = extensionName
-  new Popup({ target, props: { prop } })
+  new Popup({ target, props: {} })
 }
 
 document.addEventListener('DOMContentLoaded', render)
