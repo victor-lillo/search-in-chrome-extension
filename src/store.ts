573a23d812e3bb3db0c7945e98c1b3c440503b36
fentosv <fentos.dev@gmail.com>
2024-08-05 11:07:49 +0200

    Update async store initialization

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,12 @@
 import { writable, type Writable } from 'svelte/store'
 import type { SearchLink } from './types'
+import { STORAGE_KEYS } from './constants'
+import { getStorage } from './utils/storage'
 
-export const searchLinks: Writable<SearchLink[]> = writable()
+export const searchLinks: Writable<SearchLink[]> = writable([])
+
+export const init = (async () => {
+  const saved = await getStorage(STORAGE_KEYS.searchLinks)
+
+  searchLinks.set(saved)
+})()
