3dace12e2b8f2eca77d1229c4a0d4640d60d946b
fentosv <fentos.dev@gmail.com>
2024-08-22 13:35:50 +0200

    Update Popup UI

--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import Button from '../components/Button.svelte';
   import AddSearchLink from '../components/AddSearchLink.svelte';
   import Settings from '../components/icons/Settings.svelte';
 
@@ -8,17 +7,40 @@
   };
 </script>
 
-<section>
+<section class="form-wrapper">
   <h1>Add new query</h1>
   <AddSearchLink centerButtons={true} />
-  <Button variant="outlined-white" handleClick={openOptionsPage}>Open options <Settings /></Button>
+</section>
+<section>
+  <button class="options" type="button" on:click={openOptionsPage}>Open options <Settings /></button>
 </section>
 
 <style>
-  section {
+  .form-wrapper {
+    padding: 1rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
     min-width: var(--width-popup);
+  }
+
+  .options {
+    border: none;
+    margin-top: 0.5rem;
+    font-size: 1.2rem;
+    width: 100%;
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+    padding-block: 0.6rem;
+    background-color: var(--color-dark-3);
+    transition: background-color 0.3s ease;
+  }
+
+  @media (hover: hover) {
+    .options:hover {
+      background-color: var(--color-dark-4);
+    }
   }
 </style>
