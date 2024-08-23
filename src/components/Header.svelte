96af8ab161d3f6f84ee7cfe05f0cc9e4cf6e74f4
fentosv <fentos.dev@gmail.com>
2024-08-23 10:36:26 +0200

    Add Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Logo from './icons/Logo.svelte';
+</script>
+
+<header>
+  <h1>Search in...</h1>
+  <Logo size="56" />
+</header>
+
+<style>
+  header {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid var(--color-light-4);
+    padding-block: 0.5rem;
+  }
+</style>
