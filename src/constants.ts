2bfcd03dd67b8a3ba5e7977287b0cc57049cdabb
fentosv <fentos.dev@gmail.com>
2024-08-23 11:27:01 +0200

    Remove unused vars

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,3 @@ export const STORAGE_KEYS = {
 }
 
 export const SEARCH_PLACEHOLDER = 'PLACEHOLDER'
-
-export const MESSAGE_OBJECT = {
-  key: 'greetings',
-  value: 'hello',
-}
-
-export const RESPONSE_OK = 'ok'
