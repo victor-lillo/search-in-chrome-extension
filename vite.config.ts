e7153f158cb4c07de036de2d618d6345c8d51a9f
fentosv <fentos.dev@gmail.com>
2024-08-28 12:19:34 +0200

    Add resolve.alias field in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { crx } from '@crxjs/vite-plugin'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import manifest from './manifest'
+import path from 'path'
 
 export default defineConfig({
   plugins: [svelte(), crx({ manifest })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
