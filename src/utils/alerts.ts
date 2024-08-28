f42c8a3bb834a2235dd87b93486844ab684ba673
fentosv <fentos.dev@gmail.com>
2024-08-28 08:59:15 +0200

    Move feedback alerts to alerts.ts

---
+++ src/utils/alerts.ts
@@ -0,0 +1,24 @@
+import type { SearchLink } from '../types'
+
+const formatter = new Intl.ListFormat('en', {
+  style: 'long',
+  type: 'conjunction',
+})
+
+export function alertAllItemsAdded(added: SearchLink[]) {
+  const addedText = `✅ ${formatter.format(added.map(({ id }) => id))} added successfully.\n`
+
+  alert(addedText)
+}
+
+export function alertNoItemsAdded(noAdded: SearchLink[]) {
+  const addedText = `❌ ${formatter.format(noAdded.map(({ id }) => id))} were already saved, there is nothing to add.`
+
+  alert(addedText)
+}
+
+export function alertSomeItemsAdded(added: SearchLink[], noAdded: SearchLink[]) {
+  const addedText = `✅ ${formatter.format(added.map(({ id }) => id))} added successfully.\n❌ ${formatter.format(noAdded.map(({ id }) => id))} were already added.`
+
+  alert(addedText)
+}
