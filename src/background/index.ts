416e6e5d5cc86d72f6705dadd6b0e5762d8d4333
fentosv <fentos.dev@gmail.com>
2024-08-28 16:51:58 +0200

    Reorder background scripts

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,5 +3,5 @@ import onStorageChanged from '@/utils/onStorageChanged'
 
 // https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/api-samples/contextMenus/global_context_search/background.js
 
-onInstall()
 onStorageChanged()
+onInstall()
