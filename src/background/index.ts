088b38d4873b318a7de582488e496fbc8f89d64b
fentosv <fentos.dev@gmail.com>
2024-08-29 10:27:56 +0200

    Refactor and fix background/index.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,31 @@
-import onInstall from '@/utils/onInstall'
-import onStorageChanged from '@/utils/onStorageChanged'
+import { addContextMenu, clearContextMenu, openInNewTab } from '@/utils/contextMenu'
+import { DEFAULT } from '@/presets'
+import { setStorage } from '@/utils/storage'
+import { STORAGE_KEYS } from '@/constants'
 
 // https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/api-samples/contextMenus/global_context_search/background.js
 
-onStorageChanged()
-onInstall()
+async function setInitialStorage() {
+  console.log('Set initial storage')
+  await setStorage({ [STORAGE_KEYS.searchLinks]: DEFAULT.searchLinks })
+}
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  for (const [key, { oldValue, newValue }] of Object.entries(changes)) {
+    if (key === STORAGE_KEYS.searchLinks) {
+      console.log('Storage changed:', STORAGE_KEYS.searchLinks)
+      clearContextMenu()
+      addContextMenu(newValue)
+    }
+  }
+})
+
+chrome.runtime.onInstalled.addListener(async (details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    console.log('✅ Installed', details.reason)
+    await setInitialStorage()
+  }
+})
+
+chrome.contextMenus.onClicked.addListener(openInNewTab)
