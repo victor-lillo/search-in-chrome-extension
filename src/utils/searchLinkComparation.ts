4a46cb859eff103a773ea59ebd946eba01e5aaf1
fentosv <fentos.dev@gmail.com>
2024-08-29 17:27:25 +0200

    Add searchLinkComparation methods

---
+++ src/utils/searchLinkComparation.ts
@@ -0,0 +1,13 @@
+import type { SearchLink } from '@/types'
+
+export function added(old: SearchLink[], current: SearchLink[]) {
+  return current.filter((el) => !old.some((element) => element.id === el.id))
+}
+
+export function alreadyAdded(old: SearchLink[], current: SearchLink[]) {
+  return old.filter((el) => current.some((element) => element.id === el.id))
+}
+
+export function removed(old: SearchLink[], current: SearchLink[]) {
+  return old.filter((el) => !current.some((element) => element.id === el.id))
+}
